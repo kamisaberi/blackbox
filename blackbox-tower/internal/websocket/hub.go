7f7ad4ded5e7d95fcc732b0ae0da84ec9ecb7385
kamran saberifard <kamisaberi@gmail.com>
2025-12-12 16:58:49 +0330

    some cpp files added

--- blackbox-tower/internal/websocket/hub.go
+++ blackbox-tower/internal/websocket/hub.go
@@ -1,14 +1,61 @@
-// Run starts the infinite loop that listens for Redis messages
+package websocket
+
+import "log"
+
+// Hub maintains the set of active clients and broadcasts messages to the
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from Redis.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
 func (h *Hub) Run() {
-    for {
-        select {
-        case client := <-h.register:
-            h.clients[client] = true
-        case message := <-h.broadcast:
-            // Send the message to all connected browsers
-            for client := range h.clients {
-                client.send <- message
-            }
-        }
-    }
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+			log.Printf("[WS] Client connected. Total: %d", len(h.clients))
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+				log.Printf("[WS] Client disconnected. Total: %d", len(h.clients))
+			}
+
+		case message := <-h.broadcast:
+			// A wild alert appeared! Send it to everyone.
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					// If client's buffer is full (slow connection), drop them
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+// GetBroadcastChannel allows other modules to push messages into the hub
+func (h *Hub) GetBroadcastChannel() chan []byte {
+	return h.broadcast
 }
