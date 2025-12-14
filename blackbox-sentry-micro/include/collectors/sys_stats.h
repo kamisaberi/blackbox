ff2d964de9b4208fca3149c843dc6fc1ae2170f3
kamran saberifard <kamisaberi@gmail.com>
2025-12-14 14:49:19 +0330

    code started but not finished yet

---
+++ blackbox-sentry-micro/include/collectors/sys_stats.h
@@ -0,0 +1,8 @@
+// include/collectors/sys_stats.h
+#ifndef COLLECTORS_SYS_STATS_H
+#define COLLECTORS_SYS_STATS_H
+
+// Returns CPU usage percentage (0.0 to 100.0)
+float collector_get_cpu_usage(void);
+
+#endif
