5d48264da3c51c4dc2481343b4d19607b6f68fd2
kamran saberifard <kamisaberi@gmail.com>
2025-12-14 14:46:04 +0330

    code started but not finished yet

---
+++ blackbox-sentry-micro/include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Target Server Configuration
+#define CONFIG_SERVER_IP   "192.168.1.100" // Replace with your Blackbox Core IP
+#define CONFIG_SERVER_PORT 601             // The TCP port we defined in Core
+
+// Device Identity
+#define CONFIG_DEVICE_ID   "SENTRY-MICRO-001"
+
+#endif // CONFIG_H
